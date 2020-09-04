7bc0843657f99130b6e6de130eb4ab424faeb8ba
Jacob Short <jrshort89@gmail.com>
2020-09-04 01:46:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,19 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   ans = {}
-  data.map { |key, value| 
-      ans[value] = 0
+  data.each { |key, val| 
+    val.each { |key2, val2| 
+      val2.each { |x| 
+      if !ans.has_key? x
+        ans[x] = {key.to_s => []}
+      else
+        ans[x][key.to_s] = []
+      end
+      if ans[x].has_key? key.to_s
+        ans[x][key.to_s] << key2
+      end
+    }
   }
+  }
+  ans
 end
